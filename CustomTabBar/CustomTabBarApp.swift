ff2b2cef284647e4e7d2ae211985ea53dd1f2ae9
xNumero3 <notnumero3@gmail.com>
2024-06-03 11:29:58 +0200

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by François-Xavier on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
